c3daaf486f203a744eff559937d10278ff049c1d
Alex Anderson <alex@abanderson.com>
2018-09-04 01:54:41 -0400

    Added loading message and graphic

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,22 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Search from "./components/search";
 import Results from "./components/results";
+import Footer from "./components/footer";
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            breweries: []
+            breweries: [],
+            loading: false
         };
     }
 
     beerSearch(searchTerm) {
+        this.setState({ breweries: [], loading: true });
         Axios.get(`/api/search?q=${searchTerm}`).then(response => {
-            this.setState({ breweries: response.data });
+            this.setState({ breweries: response.data, loading: false });
         });
     }
 
@@ -26,7 +29,10 @@ class App extends Component {
                 <Navbar />
                 <div className="container">
                     <Search onSearchSubmit={this.beerSearch.bind(this)} />
-                    <Results breweries={this.state.breweries} />
+                    <Results
+                        breweries={this.state.breweries}
+                        loading={this.state.loading}
+                    />
                 </div>
             </div>
         );
--- client/src/components/results.jsx
+++ client/src/components/results.jsx
@@ -6,7 +6,25 @@ const Results = props => {
         return <Brewery key={index} brewery={brewery} />;
     });
 
-    return <div>{breweries}</div>;
+    // return <div>{breweries}</div>;
+
+    return (
+        <div>
+            {props.loading == true ? (
+                <div className="loading">
+                    <img
+                        src={require("../img/loading-beer.gif")}
+                        width="50"
+                        height="50"
+                        alt="Animated beer"
+                    />
+                    <h5>Loading...</h5>
+                </div>
+            ) : (
+                <div className="breweries">{breweries}</div>
+            )}
+        </div>
+    );
 };
 
 export default Results;
