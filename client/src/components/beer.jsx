94dcd76291b603163d60daa635e4a95f0097c579
Alex Anderson <alex@abanderson.com>
2018-09-20 22:22:29 +0800

    Added new beer-card css class

--- client/src/components/beer.jsx
+++ client/src/components/beer.jsx
@@ -3,13 +3,17 @@ import React from "react";
 const Beer = ({ beer }) => {
     return (
         // <div className="col-md-6 col-lg-4">
-        <div className="card mb-4">
+        <div className="card mb-4 beer-card">
             <div className="card-body">
                 <h5 className="card-title">{beer.beer_name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
                     {beer.beer_style}
                 </h6>
-                <img className="card-image" src={beer.beer_label} alt={`${beer.beer_name} label`} />
+                <img
+                    className="card-image"
+                    src={beer.beer_label}
+                    alt={`${beer.beer_name} label`}
+                />
                 <p className="card-text">
                     ABV: <span className="text-info">{beer.beer_abv}%</span>
                 </p>
